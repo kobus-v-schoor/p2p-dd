12cf37102723040cdae7c6f222b0fbba76dfb60c
Kobus van Schoor <v.schoor.kobus@gmail.com>
2020-01-11 18:23:01 +0200

    added networking abstractions

--- p2p-dd.py
+++ p2p-dd.py
@@ -5,12 +5,29 @@ import logging
 import socket
 import ipaddress
 import threading
+import pickle
 
 def logger(f):
     def inner(*args, **kwargs):
         name = f'{__name__}.{f.__qualname__}'
         return f(*args, **kwargs, log=logging.getLogger(name))
     return inner
+
+class SETTINGS:
+    __slots__ = ()
+
+    MESSAGE_SIZE_BYTES = 8
+    BYTE_ORDER = 'big'
+    RECEIVE_BUFFER_SIZE = 1024 # in bytes
+
+SETTINGS = SETTINGS()
+
+class MSG_TYPE:
+    __slots__ = ()
+
+    HANDSHAKE = 0
+
+MSG_TYPE = MSG_TYPE()
 
 # from https://stackoverflow.com/a/28950776
 def get_ip():
@@ -24,6 +41,83 @@ def get_ip():
     finally:
         s.close()
     return IP
+
+class ClientSocket:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+    @logger
+    def __enter__(self, log):
+        log.debug(f'initiating connection with {self.ip}:{self.port}')
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((self.ip, self.port))
+        return self
+
+    @logger
+    def __exit__(self, *args, log):
+        log.debug(f'closing connection with {self.ip}:{self.port}')
+        self.sock.close()
+
+    @logger
+    def send(self, data, log):
+        msg_size = len(data).to_bytes(SETTINGS.MESSAGE_SIZE_BYTES,
+                SETTINGS.BYTE_ORDER)
+        log.debug(f'sending data to {self.ip}:{self.port} of size {len(data)}')
+        self.sock.sendall(msg_size + data)
+
+class ServerSocket:
+    def __init__(self, csock):
+        self.csock = csock
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.csock.close()
+
+    @logger
+    def read(self, log):
+        remote_ip = self.csock.getpeername()[0]
+        log.debug(f'trying to read message from {remote_ip}')
+
+        def get(size):
+            cp = 0
+            chunks = bytearray()
+            while cp < size:
+                chunk = sock.recv(min(SETTINGS.RECEIVE_BUFFER_SIZE, size))
+                if not chunk:
+                    break
+                cp += len(chunk)
+                chunks += chunk
+            if not chunks:
+                raise RuntimeError('Connection error')
+            return bytes(chunks)
+
+        size = int.from_bytes(get(SETTINGS.MESSAGE_SIZE_BYTES),
+                SETTINGS.BYTE_ORDER)
+
+        log.debug(f'message from {remote_ip} is {size} bytes long')
+        data = get(size)
+        log.debug(f'finished reading message from {remote_ip}')
+        return data
+
+def encoder(msg_type, payload):
+    return pickle.dumps({
+        'type': msg_type,
+        'payload': payload
+        })
+
+def decoder(data):
+    data = pickle.loads(data)
+    return data['type'], data['payload']
+
+@logger
+def POST(remote, msg_type, payload, log):
+    log.debug(f'posting message of type {msg_type} to {remote}')
+
+    with ClientSocket(remote, cmd_args.port) as sock:
+        sock.send(encoder(msg_type, payload))
 
 class PeerHandshake(threading.Thread):
     def __init__(self, csock):
@@ -101,7 +195,7 @@ def main(log):
     parser.add_argument('--prefix', type=prefix_type, default=24,
             help='the network prefix')
     parser.add_argument('--port', type=port_type, default=8500,
-            help='the peer discovery server port')
+            help='the port on which to listen')
 
     parser.add_argument('--batch', action='store_true',
             help='no user interaction, assumes this node is not pre-seeded')
@@ -110,13 +204,14 @@ def main(log):
     parser.add_argument('--verbose', '-v', action='count', default=0,
             help='verbosity level, repeat to increase')
 
-    args = parser.parse_args()
+    global cmd_args
+    cmd_args = parser.parse_args()
 
-    logging.basicConfig(level=max(10, 30-(args.verbose * 10)))
-    log.debug(f'called with arguments {vars(args)}')
+    logging.basicConfig(level=max(10, 30-(cmd_args.verbose * 10)))
+    log.debug(f'called with arguments {vars(cmd_args)}')
 
-    if args.batch or args.seeded:
-        seeded = args.seeded
+    if cmd_args.batch or cmd_args.seeded:
+        seeded = cmd_args.seeded
     else:
         while True:
             ans = input('Is this node pre-seeded? [Y/n] ') or 'Y'
@@ -127,7 +222,7 @@ def main(log):
             break
         seeded = ans
 
-    peer = Peer(seeded=seeded, prefix=args.prefix, port=args.port)
+    peer = Peer(seeded=seeded, prefix=cmd_args.prefix, port=cmd_args.port)
 
 if __name__ == '__main__':
     main()
